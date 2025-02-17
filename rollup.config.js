e2e3f89bac692300b80bc962f34be90d1ac283f9
Le Vivilet <levivlet@pm.me>
2025-02-17 21:28:32 +0100

    fix: build

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import { babel } from '@rollup/plugin-babel'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 import pluginTypeScript from '@babel/preset-typescript'
 
 export default {
@@ -17,5 +18,6 @@ export default {
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
       presets: [pluginTypeScript],
     }),
+    nodeResolve(),
   ],
 }
