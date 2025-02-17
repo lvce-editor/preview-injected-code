4f120ca0eef8eb3f72628384265a42abbf859281
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-17 20:28:19 +0100

    fix: build (#7)

--- scripts/build.js
+++ scripts/build.js
@@ -71,7 +71,7 @@ delete packageJson.devDependencies
 delete packageJson.prettier
 delete packageJson.jest
 packageJson.version = version
-packageJson.main = 'dist/index.js'
+packageJson.main = 'dist/previewInjectedCodeMain.js'
 
 await writeJson(join(dist, 'package.json'), packageJson)
 
