ab007da640a305de0a1bfe6ebb65a91e2d04252c
Le Vivilet <levivlet@pm.me>
2025-02-22 21:39:28 +0100

    feature: register singleElementCondition command

--- src/parts/TestCommandMap/TestCommandMap.ts
+++ src/parts/TestCommandMap/TestCommandMap.ts
@@ -1,7 +1,10 @@
+import * as CheckSingleElementCondition from '../CheckSingleElementCondition/CheckSingleElementCondition.ts'
 import * as CreateObjectUrl from '../CreateObjectUrl/CreateObjectUrl.ts'
 import * as PerformAction from '../PerformAction/PerformAction.ts'
 
 export const testCommandMap = {
   createObjectUrl: CreateObjectUrl.createObjectUrl,
   'TestFrameWork.performAction': PerformAction.performAction,
+  'TestFrameWork.checkSingleElementCondition':
+    CheckSingleElementCondition.checkSingleElementCondition,
 }
