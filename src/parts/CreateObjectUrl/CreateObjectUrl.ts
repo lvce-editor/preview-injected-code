c2a8925f47bf39ce6aa2899107ec52056b555ba8
Le Vivilet <levivlet@pm.me>
2025-02-22 21:14:38 +0100

    feature: improve lint

--- src/parts/CreateObjectUrl/CreateObjectUrl.ts
+++ src/parts/CreateObjectUrl/CreateObjectUrl.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
 export const createObjectUrl = (blob: Blob): string => {
   const url = URL.createObjectURL(blob)
   return url
