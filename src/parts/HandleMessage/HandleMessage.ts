54f4032dba153e204e3c6bdc3bdbefa5b4a1e91a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-22 20:35:03 +0100

    feature: move handleMessageFromTestPort into separate file (#12)

--- src/parts/HandleMessage/HandleMessage.ts
+++ src/parts/HandleMessage/HandleMessage.ts
@@ -1,5 +1,6 @@
 import * as Callback from '../Callback/Callback.ts'
 import * as Command from '../Command/Command.ts'
+import * as HandleMessageFromTestPort from '../HandleMessageFromTestPort/HandleMessageFromTestPort.ts'
 import * as PortState from '../PortState/PortState.ts'
 
 const isJsonRpcResponse = (message: any) => {
@@ -23,30 +24,13 @@ const handleMessage = async (event: any) => {
   }
 }
 
-const handleMessageFromTestPort = (event: any) => {
-  // TODO invoke test function and send back result
-  const { data, target } = event
-  const { method, params, id } = data
-  if (method === 'createObjectUrl') {
-    const blob = params[0]
-    const url = URL.createObjectURL(blob)
-    target.postMessage({
-      jsonrpc: '2.0',
-      id,
-      result: url,
-    })
-  } else {
-    throw new Error('unsupported method')
-  }
-}
-
 export const handleWindowMessage = (event: any) => {
   const { data } = event
   const message = data
   const innerPort = message.params[0]
   const portType = message.params[1]
   if (portType === 'test') {
-    innerPort.onmessage = handleMessageFromTestPort
+    innerPort.onmessage = HandleMessageFromTestPort.handleMessageFromTestPort
     innerPort.postMessage('ready')
   } else {
     innerPort.onmessage = handleMessage
---
+++ src/parts/HandleMessageFromTestPort/HandleMessageFromTestPort.ts
@@ -0,0 +1,16 @@
+export const handleMessageFromTestPort = (event: any) => {
+  // TODO invoke test function and send back result
+  const { data, target } = event
+  const { method, params, id } = data
+  if (method === 'createObjectUrl') {
+    const blob = params[0]
+    const url = URL.createObjectURL(blob)
+    target.postMessage({
+      jsonrpc: '2.0',
+      id,
+      result: url,
+    })
+  } else {
+    throw new Error('unsupported method')
+  }
+}
