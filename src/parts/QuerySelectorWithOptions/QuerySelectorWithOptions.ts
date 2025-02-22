6f838eeac0d776ef41123971634f1888b7f0b51d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-22 21:05:24 +0100

    feature: add querySelector function (#29)

---
+++ src/parts/QuerySelectorWithOptions/QuerySelectorWithOptions.ts
@@ -0,0 +1,28 @@
+import { querySelector } from '../QuerySelector/QuerySelector.ts'
+
+export const querySelectorWithOptions = (
+  selector: any,
+  { nth = -1, hasText = '' } = {},
+): any => {
+  let elements = querySelector(selector)
+  if (hasText) {
+    elements = elements.filter((element) => element.textContent === hasText)
+  }
+  if (elements.length === 0) {
+    return undefined
+  }
+  if (elements.length === 1) {
+    const element = elements[0]
+    return element
+  }
+  if (nth === -1) {
+    throw new Error(
+      `too many matching elements for ${selector}, matching ${elements.length}`,
+    )
+  }
+  const element = elements[nth]
+  if (!element) {
+    throw new Error(`selector not found: ${selector}`)
+  }
+  return element
+}
