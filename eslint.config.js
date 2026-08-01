fa0a4cdb630ed8bd116006ba7b1392525a216f1f
Le Vivilet <levivilet@pm.me>
2026-08-01 16:16:48 +0000

    feature: update eslint dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
 
-export default [
+export default defineConfig([
   ...config.default,
   {
     rules: {
@@ -13,4 +14,4 @@ export default [
       'unicorn/no-useless-template-literals': 'off',
     },
   },
-]
+])
